d9c5b7c51a8e8a0886c46c95050a207f605c990d
凉凉 <xyqfer@gmail.com>
2019-10-08 14:45:36 +0800

    test zeit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('this is home page');
+});
+
+app.use(
+    '/question',
+    proxy({ 
+      target: 'https://www.zhihu.com', 
+      changeOrigin: true,
+      onProxyRes: (proxyRes, req, res) => {
+        proxyRes.on('data', (chunk) => {});
+        proxyRes.on('end', () => {
+            res.end(`
+                <style>
+                .Body--Mobile .RichContent--unescapable.is-collapsed .ContentItem-rightButton {
+                    display: none !important;
+                }
+    
+                .Body--Mobile .RichContent.is-collapsed .RichContent-inner {
+                    max-height: initial !important;
+                }
+                </style>
+            `);
+        });
+      },
+    })
+);
+
+const port = 3010;
+app.listen(port, () => {
+    console.log('Node app is running on port:', port);
+});
