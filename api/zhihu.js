c59ad162404a105a960cc10a3c36c8e4811f3330
凉凉 <xyqfer@gmail.com>
2019-10-08 15:33:46 +0800

    test

--- api/zhihu.js
+++ api/zhihu.js
@@ -4,26 +4,24 @@ module.exports = proxy({
     target: 'https://www.zhihu.com', 
     changeOrigin: true,
     pathRewrite: (path, req) => {
-        console.log(path);
         const { question } = req.query;
-        console.log(question);
         return `/question${question}`;
     },
     onProxyRes: (proxyRes, req, res) => {
-        console.log(proxyRes.headers)
-      proxyRes.on('data', (chunk) => {});
-      proxyRes.on('end', () => {
-          res.end(`
-              <style>
-              .Body--Mobile .RichContent--unescapable.is-collapsed .ContentItem-rightButton {
-                  display: none !important;
-              }
-  
-              .Body--Mobile .RichContent.is-collapsed .RichContent-inner {
-                  max-height: initial !important;
-              }
-              </style>
-          `);
-      });
+        delete proxyRes.headers['content-security-policy'];
+        proxyRes.on('data', (chunk) => {});
+        proxyRes.on('end', () => {
+            res.end(`
+                <style>
+                .Body--Mobile .RichContent--unescapable.is-collapsed .ContentItem-rightButton {
+                    display: none !important;
+                }
+    
+                .Body--Mobile .RichContent.is-collapsed .RichContent-inner {
+                    max-height: initial !important;
+                }
+                </style>
+            `);
+        });
     },
 });
